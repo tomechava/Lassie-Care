733f9871df658b47d57a71a9f5467734bc95777c
daniarango378 <darango378@gmail.com>
2024-04-15 11:54:28 -0500

    BasicDataBase

--- Lassie/models.py
+++ Lassie/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class OwnerProfile(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+class PetProfile(models.Model):
+    ownerprofile = models.ForeignKey(OwnerProfile, on_delete=models.CASCADE)
+    namePet = models.CharField(max_length=300)
+    
+    
+    def __str__(self):
+        return self.namePet
