f3811157cfa381b9e2cb1a9150f1564fc8b532ea
tomechava <techavarrg@eafit.edu.co>
2024-04-23 11:08:08 -0500

    Static dir fix + chatbot app

--- LassieCare/settings.py
+++ LassieCare/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "phonenumber_field",
-    'Lassie.apps.LassieConfig'
+    'Lassie.apps.LassieConfig',
+    'Chatbot',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    'LassieCare/static/',
+    BASE_DIR / 'static',
 ]
